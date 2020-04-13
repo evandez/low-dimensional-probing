b4cf1798f8239a0bbe7a8311d41a15f2632fab98
Evan Hernandez <evanfredhernandez@gmail.com>
2020-04-13 02:36:44 -0400

    Add real/control POS tasks.

---
+++ lodimp/tasks.py
@@ -0,0 +1,92 @@
+"""Defines real and control tasks."""
+
+import collections
+import itertools
+from typing import Callable, Dict, List, Optional
+
+import numpy as np
+import torch
+
+from lodimp import ptb
+
+# For now, tasks are defined in terms of Penn Treebank samples.
+Task = Callable[[ptb.Sample], torch.Tensor]
+
+
+class PTBRealPOS:
+    """Indexes PTB POS tags."""
+
+    def __init__(self, samples: List[ptb.Sample]):
+        """Maps each POS tag to an index.
+
+        Args:
+            samples (List[ptb.PTBSample]): The samples from which to draw tags.
+
+        """
+        self.indexer: Dict[str, int] = {}
+        for sample in samples:
+            for xpos in sample.xpos:
+                if xpos not in self.indexer:
+                    self.indexer[xpos] = len(self.indexer)
+
+    def __call__(self, sample: ptb.Sample) -> torch.Tensor:
+        """Index the part-of-speech tags for each sample.
+
+        Args:
+            samples (ptb.PTBSample): The sample to index XPOS tags for.
+
+        Returns:
+            torch.Tensor: Integer tags for each XPOS in the sample.
+
+        """
+        return torch.tensor([self.indexer[xpos] for xpos in sample.xpos])
+
+
+class PTBControlPOS:
+    """Maps words to arbitrary POS tags."""
+
+    def __init__(self,
+                 *groups: List[ptb.Sample],
+                 dist: Optional[List[float]] = None):
+        """Initialize the tagger.
+
+        The tagger computes the empirical distribution of the samples, if not
+        provided, and then uses it to generate arbitrary integer tags for each
+        individual word type.
+
+        Args:
+            *groups (List[ptb.PTBSample]): All samples, provided in one or more
+                lists, for which to generate tags.
+            dist (Optional[List[float]], optional): The empirical distribution
+                to use when sampling tags for word type. By default, is
+                computed from the list of samples.
+
+        """
+        samples = list(itertools.chain(*groups))
+        if dist is None:
+            counts: Dict[str, int] = collections.defaultdict(lambda: 0)
+            for sample in samples:
+                for pos in sample.xpos:
+                    counts[pos] += 1
+            dist = np.array([float(count) for count in counts.values()])
+            dist /= np.sum(dist)
+        assert dist is not None, 'uninitialized distribution'
+        self.dist = dist
+
+        self.tags: Dict[str, int] = {}
+        for sample in samples:
+            for word in sample.sentence:
+                if word not in self.tags:
+                    self.tags[word] = np.random.choice(len(dist), p=dist)
+
+    def __call__(self, sample: ptb.Sample) -> torch.Tensor:
+        """Tag the given samples.
+
+        Args:
+            sample (ptb.PTBSample): The sample to tag.
+
+        Returns:
+            torch.Tensor: Integer tags for every word in the sentence.
+
+        """
+        return torch.tensor([self.tags[word] for word in sample.sentence])
---
+++ lodimp/tests/tasks_test.py
@@ -0,0 +1,49 @@
+"""Unit tests for the tasks module."""
+
+import torch
+
+from lodimp import ptb, tasks
+
+SAMPLES = [
+    ptb.Sample(['foo', 'bar', 'baz'], ['A', 'B', 'C']),
+    ptb.Sample(['boof', 'foo'], ['D', 'A']),
+]
+
+REAL_TAGS = [
+    torch.tensor([0, 1, 2]),
+    torch.tensor([3, 0]),
+]
+
+
+def test_ptb_real_pos_init():
+    """Test PTBRealPOS.__init__ assigns tags as expected."""
+    task = tasks.PTBRealPOS(SAMPLES)
+    assert task.indexer == {
+        'A': 0,
+        'B': 1,
+        'C': 2,
+        'D': 3,
+    }
+
+
+def test_ptb_real_pos_call():
+    """Test PTBRealPOS.__call__ tags samples correctly."""
+    task = tasks.PTBRealPOS(SAMPLES)
+    for sample, expected in zip(SAMPLES, REAL_TAGS):
+        actual = task(sample)
+        assert torch.equal(actual, expected)
+
+
+def test_ptb_control_pos_init():
+    """Test PTBControlPOS.__init__ sets state correctly."""
+    task = tasks.PTBControlPOS(SAMPLES)
+    assert list(task.dist) == [0.4, 0.2, 0.2, 0.2]
+    assert task.tags.keys() == {'foo', 'bar', 'baz', 'boof'}
+
+
+def test_ptb_control_pos_call():
+    """Test PTBControlPOs.__call__ tags sensibly."""
+    task = tasks.PTBControlPOS(SAMPLES)
+    for sample in SAMPLES:
+        actual = task(sample)
+        assert len(actual) == len(sample.sentence)
