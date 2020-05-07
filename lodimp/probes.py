975ea399a9d7daa3bb4ff1e90a7174478bac2db1
Evan Hernandez <evanfredhernandez@gmail.com>
2020-05-07 01:18:38 -0400

    Add ProjectedMLP probe.

--- lodimp/probes.py
+++ lodimp/probes.py
@@ -1,5 +1,7 @@
 """Defines probe architectures."""
 
+from typing import Optional
+
 import torch
 from torch import nn
 
@@ -9,25 +11,68 @@ class Projection(nn.Linear):
 
 
 class ProjectedLinear(nn.Module):
-    """Probe for low-dimensional subspace of representation."""
+    """Linear probe in a low-dimensional subspace of representation."""
 
-    def __init__(self, input_dimension: int, hidden_dimension: int,
+    def __init__(self, input_dimension: int, projected_dimension: int,
                  classes: int):
         """Initilize the architecture.
 
         Args:
             input_dimension (int): Dimensionality of input vectors.
-            hidden_dimension (int): Dimensionality of projected space.
+            projected_dimension (int): Dimensionality of projected space.
             classes (int): Number of classes to predict for each vector.
 
         """
         super().__init__()
-        self.project = Projection(input_dimension, hidden_dimension)
-        self.classify = nn.Linear(hidden_dimension, classes)
+        self.project = Projection(input_dimension, projected_dimension)
+        self.classify = nn.Linear(projected_dimension, classes)
         # Softmax is implicit in loss functions.
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         """Project the inputs to low dimension and predict labels for them.
+
+        Args:
+            inputs (torch.Tensor): Shape (B, H) tensor, where B is batch size
+                and H is hidden_dimension.
+
+        Returns:
+            torch.Tensor: Shape (B, C) tensor containing class logits for each
+                sample in batch.
+
+        """
+        return self.classify(self.project(inputs))
+
+
+class ProjectedMLP(nn.Module):
+    """MLP probe in a low-dimensional subspace of the representation."""
+
+    def __init__(self,
+                 input_dimension: int,
+                 projected_dimension: int,
+                 classes: int,
+                 hidden_dimension: Optional[int] = None):
+        """Initilize the architecture.
+
+        Args:
+            input_dimension (int): Dimensionality of input vectors.
+            projected_dimension (int): Dimensionality of projected space.
+            classes (int): Number of classes to predict for each vector.
+            hidden_dimension (int, optional): Dimensionality of MLP hidden
+                layer. Defaults to projected_dimension.
+
+        """
+        super().__init__()
+        self.project = Projection(input_dimension, projected_dimension)
+        hidden_dimension = hidden_dimension or projected_dimension
+        self.classify = nn.Sequential(
+            nn.Linear(projected_dimension, hidden_dimension),
+            nn.ReLU(),
+            nn.Linear(hidden_dimension, classes),
+            # Softmax is implicit in loss functions.
+        )
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        """Project the inputs to low dimension and predict labels with an MLP.
 
         Args:
             inputs (torch.Tensor): Shape (B, H) tensor, where B is batch size
--- lodimp/tests/probes_test.py
+++ lodimp/tests/probes_test.py
@@ -14,7 +14,7 @@ SEQ_LENGTH = 15
 
 @pytest.fixture
 def projected_linear():
-    """Returns a Projection for testing."""
+    """Returns a ProjectedLinear probe for testing."""
     return probes.ProjectedLinear(INPUT_DIM, PROJECTION_DIM, CLASSES)
 
 
@@ -22,4 +22,17 @@ def test_projected_linear_forward(projected_linear):
     """Tests ProjectedLinear.forward returns tensor of correct dimension."""
     inputs = torch.randn(BATCH_SIZE, SEQ_LENGTH, INPUT_DIM)
     actual = projected_linear(inputs)
+    assert actual.shape == (BATCH_SIZE, SEQ_LENGTH, CLASSES)
+
+
+@pytest.fixture
+def projected_mlp():
+    """Returns a ProjectedMLP probe for testing."""
+    return probes.ProjectedMLP(INPUT_DIM, PROJECTION_DIM, CLASSES)
+
+
+def test_projected_mlp_forward(projected_mlp):
+    """Tests ProjectedMLP.forward returns tensor of correct dimension."""
+    inputs = torch.randn(BATCH_SIZE, SEQ_LENGTH, INPUT_DIM)
+    actual = projected_mlp(inputs)
     assert actual.shape == (BATCH_SIZE, SEQ_LENGTH, CLASSES)
