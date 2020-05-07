529f7b786aa4d981f8cd0c1d6e37019104de95b5
Evan Hernandez <evanfredhernandez@gmail.com>
2020-05-07 01:06:53 -0400

    Add truncate() test.

--- lodimp/tests/linalg_test.py
+++ lodimp/tests/linalg_test.py
@@ -10,3 +10,10 @@ def test_effective_rank():
     matrix = torch.diag(torch.tensor([100, 100, 1e-6]))
     actual = linalg.effective_rank(matrix)
     assert torch.allclose(torch.tensor(actual), torch.tensor(2.))
+
+
+def test_truncate():
+    """Test truncate matches intuition."""
+    matrix = torch.eye(3)
+    actual = linalg.truncate(matrix, 2)
+    assert torch.matrix_rank(actual) == 2
