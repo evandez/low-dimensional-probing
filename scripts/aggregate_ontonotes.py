e7eae6b6f20b7a26e2606259f9356b554763ccbe
Evan Hernandez <evanfredhernandez@gmail.com>
2020-05-17 22:53:03 -0400

    Add script for aggregating Ontonotes annotations.

---
+++ scripts/aggregate_ontonotes.py
@@ -0,0 +1,42 @@
+"""Aggregate the WSJ portion of Ontonotes.
+
+Annotations can be retrieved from, e.g., here:
+https://github.com/yuchenlin/OntoNotes-5.0-NER-BIO/
+"""
+
+import argparse
+import glob
+import logging
+import pathlib
+import sys
+
+parser = argparse.ArgumentParser(description='Aggregate Ontonotes WSJ data.')
+parser.add_argument('data', type=pathlib.Path, help='Path to data root.')
+parser.add_argument(
+    '--out',
+    type=pathlib.Path,
+    help='Output directory for aggregatation. Defaults to data directory.')
+parser.add_argument('--quiet',
+                    action='store_const',
+                    dest='log_level',
+                    const=logging.WARNING,
+                    default=logging.INFO,
+                    help='Suppress logging output to warnings and above.')
+options = parser.parse_args()
+
+logging.basicConfig(stream=sys.stdout,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=options.log_level)
+
+root = options.data / 'conll-formatted-ontonotes-5.0' / 'data'
+for split in ('train', 'development', 'test'):
+    glob_path = root / split / 'data/english/annotations/nw/wsj/*/*.gold_conll'
+    split = 'dev' if split == 'development' else split
+    out_path = (options.out or options.data) / f'ontonotes5-wsj-{split}.conll'
+    files = glob.glob(str(glob_path))
+    with open(out_path, 'w') as out:
+        for file in files:
+            logging.info('%s set: appending %s', split, file)
+            with open(file, 'r') as handle:
+                out.writelines(handle.readlines()[1:-1])
