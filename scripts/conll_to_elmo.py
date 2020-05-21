430e9033113a067c2bef66bb28424a9a26b4b2c0
Evan Hernandez <evanfredhernandez@gmail.com>
2020-05-21 13:03:28 -0400

    Fix bug where newlines not inserted.
    Also use correct ELMo model and test it for real.

--- scripts/conll_to_elmo.py
+++ scripts/conll_to_elmo.py
@@ -9,6 +9,13 @@ from typing import List
 
 from torch import cuda
 
+ELMO_URL = 'https://s3-us-west-2.amazonaws.com/allennlp/models/elmo'
+ELMO_MODEL = '2x4096_512_2048cnn_2xhighway_5.5B'
+WEIGHTS_FILE = 'elmo_2x4096_512_2048cnn_2xhighway_5.5B_weights.hdf5'
+WEIGHTS_URL = f'{ELMO_URL}/{ELMO_MODEL}/{WEIGHTS_FILE}'
+OPTIONS_FILE = 'elmo_2x4096_512_2048cnn_2xhighway_5.5B_options.json'
+OPTIONS_URL = f'{ELMO_URL}/{ELMO_MODEL}/{OPTIONS_FILE}'
+
 parser = argparse.ArgumentParser(description='Pre-embed sentences with ELMo.')
 parser.add_argument('data', type=pathlib.Path, help='Path to .conll(x) file.')
 parser.add_argument('out', type=pathlib.Path, help='Path to output file.')
@@ -39,8 +46,11 @@ if buffer:
 with tempfile.TemporaryDirectory() as tempdir:
     raw = pathlib.Path(tempdir) / f'{options.data.name}.raw'
     with raw.open('w') as handle:
-        handle.writelines(sentences)
-    command = ['allennlp', 'elmo', '--all', str(raw), str(options.out)]
+        handle.write('\n'.join(sentences))
+    command = ['allennlp', 'elmo', str(raw), str(options.out)]
+    command += ['--weight-file', WEIGHTS_URL]
+    command += ['--options-file', OPTIONS_URL]
+    command += ['--all']
     if cuda.is_available():
         command += ['--cuda-device', '0']
     process = subprocess.run(command)
---
+++ scripts/test_conll_to_elmo.py
@@ -0,0 +1,20 @@
+"""Sanity check that we are using same ELMo as Hewitt paper."""
+
+import argparse
+import pathlib
+import h5py
+import torch
+
+parser = argparse.ArgumentParser(description='Test conll_to_elmo.py script.')
+parser.add_argument('actual', type=pathlib.Path, help='Generated hdf5.')
+parser.add_argument('expected', type=pathlib.Path, help='Expected hdf5.')
+options = parser.parse_args()
+
+actuals = h5py.File(options.actual, 'r')
+expecteds = h5py.File(options.expected, 'r')
+
+assert len(actuals.keys()) == len(expecteds.keys())
+for index in range(len(expecteds) - 1):
+    actual = torch.tensor(actuals[str(index)][:])
+    expected = torch.tensor(expecteds[str(index)][:])
+    assert torch.allclose(actual, expected, atol=2e-4)
