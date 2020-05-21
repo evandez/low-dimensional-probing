a843f3db829f1c79f405fb800ba784f423b5cc6a
Evan Hernandez <evanfredhernandez@gmail.com>
2020-05-21 15:17:47 -0400

    splits_to_* handle PTB and Ontonotes.

--- scripts/splits_to_bert.py
+++ scripts/splits_to_bert.py
@@ -5,9 +5,26 @@ import pathlib
 import subprocess
 import sys
 
+DATASETS = ('ptb', 'ontonotes')
+
+PREFIXES = {
+    'ptb': 'ptb3-wsj-',
+    'ontonotes': 'ontonotes5-',
+}
+assert PREFIXES.keys() == set(DATASETS)
+
+WORD_COLUMNS = {
+    'ptb': 1,
+    'ontonotes': 3,
+}
+assert WORD_COLUMNS.keys() == set(DATASETS)
+
 parser = argparse.ArgumentParser(description='Embed dataset with BERT.')
 parser.add_argument('data', type=pathlib.Path, help='Path to data spltis.')
-parser.add_argument('--prefix', default='ptb3-wsj-', help='File prefixes.')
+parser.add_argument('--dataset',
+                    choices=DATASETS,
+                    default='ptb',
+                    help='Dataset to embed.')
 parser.add_argument('--pretrained',
                     default='bert-base-uncased',
                     help='Forwarded to conll_to_bert.')
@@ -18,9 +35,12 @@ for split in ('train', 'dev', 'test'):
     command = [
         'python3',
         str(script),
-        str(options.data / f'{options.prefix}{split}.conllx'),
+        str(options.data / f'{PREFIXES[options.dataset]}{split}.conllx'),
         str(options.data / f'raw.{split}.{options.pretrained}-layers.hdf5'),
-        '--pretrained', options.pretrained
+        '--pretrained',
+        options.pretrained,
+        '--word-column',
+        str(WORD_COLUMNS[options.dataset]),
     ]
     print(' '.join(command))
     process = subprocess.run(command)
