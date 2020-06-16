1f191fff02a7acb4601f849072e8479220e55be2
Evan Hernandez <evanfredhernandez@gmail.com>
2020-06-16 17:14:10 -0400

    Add DLP submodule.

---
+++ lodimp/tasks/dlp.py
@@ -0,0 +1,231 @@
+"""Core experiments for the dependency label prediction task."""
+
+import collections
+import itertools
+from typing import Dict, Iterator, Optional, Sequence, Tuple, Union
+
+from lodimp.common import tasks
+from lodimp.common.data import ptb, representations as reps
+
+import numpy as np
+import torch
+
+UNK = 'unk'
+
+
+class DLPIndexer:
+    """Maps pairs of words to their syntactic relationship, if any."""
+
+    def __init__(self, samples: Sequence[ptb.Sample], unk: str = UNK):
+        """Map each relation label to an integer.
+
+        Args:
+            samples (Sequence[ptb.Sample]): The samples from which to determine
+                possible relations.
+            unk (str): Label to use when un-indexed dependency label is
+                encountered.
+
+        """
+        labels = {rel for sample in samples for rel in sample.relations}
+        self.indexer = {unk: 0}
+        for label in sorted(labels):
+            self.indexer[label] = len(self.indexer)
+        self.unk = unk
+
+    def __call__(self, sample: ptb.Sample) -> torch.Tensor:
+        """Map all possible (word, word) pairs to labels.
+
+        Args:
+            sample (ptb.Sample): The sample to label.
+
+        Returns:
+            torch.Tensor: For length W sentence, returns shape (W, W) matrix
+                where element (v, w) is the index of the label describing
+                the relationship between word v and w, if any. Defaults to
+                the "unk" label, even if there is no relationship between
+                v and w.
+
+        """
+        heads, relations = sample.heads, sample.relations
+        labels = torch.empty(len(heads), len(heads), dtype=torch.long)
+        labels.fill_(self.indexer[self.unk])
+        for word, (head, rel) in enumerate(zip(heads, relations)):
+            if head == -1:
+                labels[word, word] = self.indexer[rel]
+            else:
+                label = self.indexer.get(rel, self.indexer[self.unk])
+                labels[word, head] = label
+        return labels
+
+    def __len__(self) -> int:
+        """Returns the number of unique labels for this task."""
+        return len(self.indexer)
+
+
+class ControlDLPIndexer:
+    """Maps pairs of words to arbitrary syntactic relationships."""
+
+    def __init__(self,
+                 *groups: Sequence[ptb.Sample],
+                 dist: Optional[Sequence[float]] = None):
+        """Map each relation label to an arbitrary (integer) label.
+
+        We only do this for pairs of words which have a head-dependent
+        relationship in the original dataset.
+
+        Args:
+            *groups (Sequence[ptb.Samples]): The samples from which to pull
+                possible word pairs.
+            dist (Optional[Sequence[float]], optional): The empirical
+                distribution to use when sampling tags per word type.
+                By default, is computed from the list of samples.
+
+        """
+        samples = tuple(itertools.chain(*groups))
+
+        if dist is None:
+            counts: Dict[str, int] = collections.defaultdict(lambda: 0)
+            for sample in samples:
+                for relation in sample.relations:
+                    counts[relation] += 1
+            dist = np.array([float(count) for count in counts.values()])
+            dist /= np.sum(dist)
+        assert dist is not None, 'uninitialized distribution?'
+        self.dist = dist
+
+        self.rels: Dict[Tuple[str, str], int] = {}
+        for sample in samples:
+            sentence = sample.sentence
+            heads = sample.heads
+            for dep, head in enumerate(heads):
+                if head == -1:
+                    head = dep
+                words = (sentence[dep], sentence[head])
+                if words not in self.rels:
+                    # Add one so that 0 is reserved for "no relationship" tag.
+                    self.rels[words] = np.random.choice(len(dist), p=dist) + 1
+
+    def __call__(self, sample: ptb.Sample) -> torch.Tensor:
+        """Map all possible (word, word) pairs to labels.
+
+        Args:
+            sample (ptb.Sample): The sample to label.
+
+        Returns:
+            torch.Tensor: For length W sentence, returns shape (W, W) matrix
+                where element (v, w) is the index of the label describing
+                the relationship between word v and w, if any. Defaults to
+                the "unk" label, even if there is no relationship between
+                v and w.
+
+        """
+        heads = sample.heads
+        labels = torch.zeros(len(heads), len(heads), dtype=torch.long)
+        for dep, head in enumerate(heads):
+            if head == -1:
+                head = dep
+            words = (sample.sentence[dep], sample.sentence[head])
+            labels[dep, head] = self.rels.get(words, 0)
+        return labels
+
+    def __len__(self) -> int:
+        """Returns the number of relationships, including the null one."""
+        return len(self.dist) + 1
+
+
+class DLPTaskDataset(tasks.TaskDataset):
+    """Iterates over (word representation pair, dependency label) pairs."""
+
+    def __init__(
+        self,
+        representations: reps.RepresentationLayerDataset,
+        annotations: Sequence[ptb.Sample],
+        indexer: Union[DLPIndexer, ControlDLPIndexer],
+    ):
+        """Initializes dataset by mapping each dependency label to an index.
+
+        Args:
+            representations (representations.RepresentationsLayerDataset): Word
+                representations corresponding to the words to be paired and
+                labeled.
+            annotations (Sequence[ptb.PTBSample]): The PTB annotations from
+                which to pull dependency labels.
+            indexer (Union[DLPIndexer, ControlDLPIndexer]): Callable mapping
+                PTB dependency label annotations to integer tensors.
+
+        Raises:
+            ValueError: If number of representations/annotations do not match.
+
+        """
+        if len(representations) != len(annotations):
+            raise ValueError(f'got {len(representations)} representations '
+                             f'but {len(annotations)} annotations')
+
+        self.representations = representations
+        self.annotations = annotations
+        self.indexer = indexer
+
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Return (representations, integral POS tags) for index'th sentence.
+
+        Args:
+            index (int): Index of the sentence in the dataset.
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: First tensor is shape
+                (sentence_length, representation_dimension) containing word
+                representations, and second is shape (sentence_length,)
+                containing integral POS tags.
+
+        """
+        representations = self.representations[index]
+        annotations = self.annotations[index]
+        assert len(representations) == len(
+            annotations.sentence), 'diff sentence lengths?'
+        rels = self.indexer(annotations)
+
+        # Find all pairs of words sharing an edge.
+        indexes = set(range(len(representations)))
+        pairs = [(i, j) for i in indexes for j in indexes if rels[i, j]]
+        assert pairs and len(pairs) == len(representations), 'missing edges?'
+
+        # Stack everything before returning it.
+        bigrams = torch.stack([
+            torch.stack((representations[i], representations[j]))
+            for i, j in pairs
+        ])
+        labels = torch.stack([rels[i, j] for i, j in pairs])
+
+        return bigrams, labels
+
+    def __iter__(self) -> Iterator[Tuple[torch.Tensor, torch.Tensor]]:
+        """Yields all (sentence representations, sentence POS tags) samples."""
+        for index in range(len(self)):
+            yield self[index]
+
+    def __len__(self) -> int:
+        """Returns the number of sentences (batches) in the dataset."""
+        return len(self.annotations)
+
+    @property
+    def sample_representations_shape(self) -> Sequence[int]:
+        """Returns the dimensionality of the representation pairs."""
+        return (2, self.representations.dataset.dimension)
+
+    @property
+    def sample_features_shape(self) -> Sequence[int]:
+        """Returns the shape of each individual POS tag.
+
+        Since POS tags are integral scalars, there is no such shape!
+        """
+        return ()
+
+    def count_samples(self) -> int:
+        """Returns the number of words in the dataset."""
+        return sum(
+            self.representations.dataset.length(index)
+            for index in range(len(self.representations)))
+
+    def count_unique_features(self) -> int:
+        """Returns number of unique POS seen in data."""
+        return len(self.indexer)
---
+++ lodimp/tests/tasks/dlp_test.py
@@ -0,0 +1,231 @@
+"""Unit tests for tasks/dlp module."""
+
+import pathlib
+import tempfile
+
+from lodimp.common.data import ptb, representations
+from lodimp.tasks import dlp
+
+import h5py
+import pytest
+import torch
+
+ROOT = 'root'
+LABELS = (
+    'label-a',
+    'label-b',
+    ROOT,
+)
+
+WORDS = (
+    'word-a',
+    'word-b',
+    'word-c',
+    'word-d',
+    'word-e',
+)
+
+SAMPLES = (
+    ptb.Sample(
+        (WORDS[0], WORDS[1], WORDS[2]),
+        ('irrelevant', 'irrelevant', 'irrelevant'),
+        (2, 0, -1),
+        (LABELS[0], LABELS[1], ROOT),
+    ),
+    ptb.Sample(
+        (WORDS[3], WORDS[4]),
+        ('irrelevant', 'irrelevant'),
+        (-1, 0),
+        (ROOT, LABELS[0]),
+    ),
+)
+SEQ_LENGTHS = tuple(len(sample.sentence) for sample in SAMPLES)
+
+INDEXED_LABELS = (
+    torch.tensor([
+        [0, 0, 1],
+        [2, 0, 0],
+        [0, 0, 3],
+    ]),
+    torch.tensor([
+        [3, 0],
+        [1, 0],
+    ]),
+)
+
+
+@pytest.fixture
+def dlp_indexer():
+    """Returns a DLPIndexer for testing."""
+    return dlp.DLPIndexer(SAMPLES)
+
+
+def test_dlp_indexer_init(dlp_indexer):
+    """Test DLPIndexer.__init__ maps labels to integers correctly."""
+    assert dlp_indexer.indexer == {
+        dlp.UNK: 0,
+        LABELS[0]: 1,
+        LABELS[1]: 2,
+        LABELS[2]: 3,
+    }
+
+
+def test_dlp_indexer_call(dlp_indexer):
+    """Test DLPIndexer.__call__ returns correct label matrix."""
+    for sample, expected in zip(SAMPLES, INDEXED_LABELS):
+        actual = dlp_indexer(sample)
+        assert torch.equal(actual, expected)
+
+
+def test_dlp_indexer_len(dlp_indexer):
+    """Test DLPIndexer.__len__ returns number of labels."""
+    assert len(dlp_indexer) == len(LABELS) + 1
+
+
+@pytest.fixture
+def control_dlp_indexer():
+    """Returns a ControlDLPIndexer for testing."""
+    return dlp.ControlDLPIndexer(SAMPLES)
+
+
+def test_control_dlp_indexer_init(control_dlp_indexer):
+    """Test ControlDLPIndexer.__init__ maps labels to integers."""
+    assert 0 not in control_dlp_indexer.rels
+    assert len(control_dlp_indexer.dist) == 3
+    assert len(control_dlp_indexer.rels) == 5
+
+
+def test_control_dlp_indexer_call(control_dlp_indexer):
+    """Test ControlDLPIndexer.__call__ returns reasonable labels."""
+    for sample, expected in zip(SAMPLES, INDEXED_LABELS):
+        actual = control_dlp_indexer(sample)
+        assert actual.shape == expected.shape
+        assert actual[expected == 0].eq(0).all()
+        assert not actual[expected != 0].eq(0).any()
+
+
+def test_control_dlp_indexer_call_deterministic(control_dlp_indexer):
+    """Test ControlDLPIndexer.__call__ is deterministic."""
+    expecteds = [control_dlp_indexer(sample) for sample in SAMPLES]
+    actuals = [control_dlp_indexer(sample) for sample in SAMPLES]
+    for actual, expected in zip(actuals, expecteds):
+        assert actual.equal(expected)
+
+
+def test_control_dlp_indexer_len(control_dlp_indexer):
+    """Test ControlDLPIndexer.__len__ returns correct length."""
+    task = dlp.ControlDLPIndexer(SAMPLES)
+    assert len(task) == len(LABELS) + 1
+
+
+N_LAYERS = 3
+N_DIMS_PER_REP = 10
+
+
+@pytest.fixture
+def reps():
+    """Returns fake representations for testing."""
+    return tuple(
+        torch.randn(N_LAYERS, seq_length, N_DIMS_PER_REP)
+        for seq_length in SEQ_LENGTHS)
+
+
+LAYER = 0
+
+
+@pytest.yield_fixture
+def rep_layer_dataset(reps):
+    """Yields the path to a standard h5 file containing representations."""
+    with tempfile.TemporaryDirectory() as tempdir:
+        path = pathlib.Path(tempdir) / 'reps.h5'
+        with h5py.File(path, 'w') as file:
+            for index, reps in enumerate(reps):
+                file.create_dataset(str(index), data=reps)
+            file.create_dataset('ignore', data=0)
+        yield representations.RepresentationDataset(path).layer(LAYER)
+
+
+@pytest.fixture
+def dlp_task_dataset(rep_layer_dataset, dlp_indexer):
+    """Returns a DLPTaskDataset for testing."""
+    return dlp.DLPTaskDataset(rep_layer_dataset, SAMPLES, dlp_indexer)
+
+
+@pytest.fixture
+def rep_pairs(rep_layer_dataset):
+    """Returns the expected representation pairs for each sentence."""
+    return (
+        torch.stack((
+            torch.stack((rep_layer_dataset[0][0], rep_layer_dataset[0][2])),
+            torch.stack((rep_layer_dataset[0][1], rep_layer_dataset[0][0])),
+            torch.stack((rep_layer_dataset[0][2], rep_layer_dataset[0][2])),
+        )),
+        torch.stack((
+            torch.stack((rep_layer_dataset[1][0], rep_layer_dataset[1][0])),
+            torch.stack((rep_layer_dataset[1][1], rep_layer_dataset[1][0])),
+        )),
+    )
+
+
+FLATTENED_INDEXED_LABELS = (torch.tensor((1, 2, 3)), torch.tensor((3, 1)))
+
+
+def test_dlp_task_dataset_iter(dlp_task_dataset, rep_pairs):
+    """Test DLPTaskDataset.__iter__ yields all samples."""
+    actuals = list(iter(dlp_task_dataset))
+
+    expecteds = list(zip(rep_pairs, FLATTENED_INDEXED_LABELS))
+    for actual, expected in zip(actuals, expecteds):
+        assert len(actual) == 2
+        assert len(expected) == 2, 'sanity check!'
+
+        actual_reps, actual_tags = actual
+        expected_reps, expected_tags = expected
+        assert actual_reps.equal(expected_reps)
+        assert actual_tags.equal(expected_tags)
+
+
+def test_dlp_task_dataset_getitem(dlp_task_dataset, rep_pairs):
+    """Test DLPTaskDataset.__getitem__ returns correct samples."""
+    expecteds = list(zip(rep_pairs, FLATTENED_INDEXED_LABELS))
+    for index, expected in enumerate(expecteds):
+        assert len(expected) == 2, 'sanity check!'
+        expected_reps, expected_tags = expected
+
+        actual = dlp_task_dataset[index]
+        assert len(actual) == 2
+
+        actual_reps, actual_tags = actual
+        assert actual_reps.equal(expected_reps)
+        assert actual_tags.equal(expected_tags)
+
+
+def test_dlp_task_dataset_len(dlp_task_dataset):
+    """Test DLPTaskDataset.__len__ returns number of sentences."""
+    return len(dlp_task_dataset) == len(SEQ_LENGTHS)
+
+
+def test_dlp_task_dataset_sample_representations_shape(dlp_task_dataset):
+    """Test DLPTaskDataset.sample_representations_shape returns rep dim."""
+    assert dlp_task_dataset.sample_representations_shape == (2, N_DIMS_PER_REP)
+
+
+def test_dlp_task_dataset_sample_features_shape(dlp_task_dataset):
+    """Test DLPTaskDataset.sample_features_shape returns ()."""
+    assert dlp_task_dataset.sample_features_shape == ()
+
+
+def test_dlp_task_dataset_count_samples(dlp_task_dataset):
+    """Test DLPTaskDataset.count_samples returns number of words."""
+    assert dlp_task_dataset.count_samples() == sum(SEQ_LENGTHS)
+
+
+def test_dlp_task_dataset_count_unique_features(dlp_task_dataset):
+    """Test DLPTaskDataset.count_samples returns number of words."""
+    assert dlp_task_dataset.count_unique_features() == len(LABELS) + 1
+
+
+def test_dlp_task_dataset_init_bad_lengths(rep_layer_dataset, dlp_indexer):
+    """Test DLPTaskDataset.__init__ dies when given different reps/annos."""
+    with pytest.raises(ValueError, match='.*1 annotation.*'):
+        dlp.DLPTaskDataset(rep_layer_dataset, SAMPLES[:1], dlp_indexer)
