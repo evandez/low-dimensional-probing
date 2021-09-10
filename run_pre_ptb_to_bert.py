efb864955af985312c723bf96ad3a48f58cbe37b
Evan Hernandez <evanfredhernandez@gmail.com>
2021-09-10 16:28:51 -0400

    Save random BERT weights.

--- run_pre_ptb_to_bert.py
+++ run_pre_ptb_to_bert.py
@@ -4,6 +4,9 @@ import json
 import pathlib
 from typing import List
 
+from lodimp.parse import splits
+from lodimp.utils import logging
+
 import h5py
 import numpy as np
 import torch
@@ -25,6 +28,13 @@ parser.add_argument('--word-column',
 parser.add_argument('--random',
                     action='store_true',
                     help='randomly initialize bert')
+parser.add_argument('--bert-config',
+                    default='bert-base-uncased',
+                    help='pretrained bert config (default: bert-base-uncased)')
+parser.add_argument('--splits',
+                    nargs='+',
+                    default=splits.STANDARD_SPLITS,
+                    help='splits to process (default: train, dev, test)')
 parser.add_argument('--device', help='use this device (default: guessed)')
 args = parser.parse_args()
 
@@ -33,8 +43,28 @@ if not data_dir.exists():
     raise FileNotFoundError(f'data dir {data_dir} not found')
 out_dir = args.out_dir or data_dir
 
+logging.configure()
+log = logging.getLogger(__name__)
+
 device = args.device or 'cuda' if cuda.is_available() else 'cpu'
 
+# Load the model.
+tokenizer = transformers.BertTokenizer.from_pretrained(args.bert_config)
+if args.random:
+    random_weights_dir = out_dir / 'bert-base-uncased-random'
+    if random_weights_dir.exists():
+        bert = transformers.BertModel.from_pretrained(str(random_weights_dir))
+        config = bert.config
+    else:
+        config = transformers.BertConfig.from_pretrained(args.bert_config)
+        bert = transformers.BertModel(config)
+else:
+    bert = transformers.BertModel.from_pretrained(args.bert_config)
+    config = bert.config
+assert isinstance(config, transformers.BertConfig)
+bert.to(device).eval()
+
+# Process each split.
 for split in ('train', 'dev', 'test'):
     data_file = data_dir / f'ptb3-wsj-{split}.conllx'
     out_file = out_dir / f'bert{"-random" if args.random else ""}-{split}.h5'
@@ -54,16 +84,6 @@ for split in ('train', 'dev', 'test'):
             partial.append(components[args.word_column])
         if partial:
             sentences.append(tuple(partial))
-
-    tokenizer = transformers.BertTokenizer.from_pretrained('bert-base-uncased')
-    if args.random:
-        config = transformers.BertConfig.from_pretrained('bert-base-uncased')
-        bert = transformers.BertModel(config)
-    else:
-        bert = transformers.BertModel.from_pretrained('bert-base-uncased')
-        config = bert.config
-    assert isinstance(config, transformers.BertConfig)
-    bert.to(device).eval()
 
     with h5py.File(str(out_file), 'w') as handle:
         for index, sentence in tqdm(tuple(enumerate(sentences)),
