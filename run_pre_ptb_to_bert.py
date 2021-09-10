242325d6a2b6544cfd6fefb28d8b904b7ec1d408
Evan Hernandez <evanfredhernandez@gmail.com>
2021-09-10 15:52:29 -0400

    Merge some pre scripts.

---
+++ run_pre_ptb_to_bert.py
@@ -0,0 +1,94 @@
+"""Embed all sentences in the .conllx file with BERT."""
+import argparse
+import json
+import pathlib
+from typing import List
+
+import h5py
+import numpy as np
+import torch
+import transformers
+from torch import cuda
+from tqdm.auto import tqdm
+
+parser = argparse.ArgumentParser(description='pre-embed sentences with bert')
+parser.add_argument('data_dir',
+                    type=pathlib.Path,
+                    help='dir containing .conll(x) files')
+parser.add_argument('--out-dir',
+                    type=pathlib.Path,
+                    help='output dir (default: data_dir)')
+parser.add_argument('--word-column',
+                    type=int,
+                    default=1,
+                    help='index of .conll column containing words')
+parser.add_argument('--random',
+                    action='store_true',
+                    help='randomly initialize bert')
+parser.add_argument('--device', help='use this device (default: guessed)')
+args = parser.parse_args()
+
+data_dir = args.data_dir
+if not data_dir.exists():
+    raise FileNotFoundError(f'data dir {data_dir} not found')
+out_dir = args.out_dir or data_dir
+
+device = args.device or 'cuda' if cuda.is_available() else 'cpu'
+
+for split in ('train', 'dev', 'test'):
+    data_file = data_dir / f'ptb3-wsj-{split}.conllx'
+    out_file = out_dir / f'bert{"-random" if args.random else ""}-{split}.h5'
+
+    # TODO(evandez): Use parse lib?
+    sentences = []
+    partial: List[str] = []
+    with data_file.open('r') as conll:
+        for line in conll:
+            line = line.strip()
+            if not line and partial:
+                sentences.append(tuple(partial))
+                partial = []
+            if not line or line.startswith('#'):
+                continue
+            components = line.split()
+            partial.append(components[args.word_column])
+        if partial:
+            sentences.append(tuple(partial))
+
+    tokenizer = transformers.BertTokenizer.from_pretrained('bert-base-uncased')
+    if args.random:
+        config = transformers.BertConfig.from_pretrained('bert-base-uncased')
+        bert = transformers.BertModel(config)
+    else:
+        bert = transformers.BertModel.from_pretrained('bert-base-uncased')
+        config = bert.config
+    assert isinstance(config, transformers.BertConfig)
+    bert.to(device).eval()
+
+    with h5py.File(str(out_file), 'w') as handle:
+        for index, sentence in tqdm(tuple(enumerate(sentences)),
+                                    desc='embed sentences'):
+            tokens = tokenizer.encode(sentence, add_special_tokens=False)
+            dataset = handle.create_dataset(
+                str(index),
+                shape=(config.num_hidden_layers + 1, len(tokens),
+                       config.hidden_size),
+                dtype='f',
+            )
+
+            inputs = torch.tensor([tokens], device=device)
+            with torch.no_grad():
+                outputs = bert(inputs, output_hidden_states=True)
+                hiddens = outputs.hidden_states
+                assert len(hiddens) == 13
+                embeddings = torch.cat(hiddens)
+                assert embeddings.shape == (13, len(tokens), 768)
+
+            dataset[:] = embeddings.cpu().numpy()
+
+        # Create a sentence_to_index item so bert h5 files look like ELMo ones.
+        joined = [' '.join(sentence) for sentence in sentences]
+        sentence_to_index = {sentence: i for i, sentence in enumerate(joined)}
+        sentence_to_index_str = json.dumps(sentence_to_index).encode()
+        out_file.create_dataset('sentence_to_index',
+                                data=np.array([sentence_to_index_str]))
---
+++ run_pre_ptb_to_elmo.py
@@ -0,0 +1,90 @@
+"""Embed all sentences in the .conllx file with ELMo."""
+import argparse
+import pathlib
+import subprocess
+import sys
+import tempfile
+from typing import List
+
+from torch import cuda
+
+EPILOG = '''\
+WARNING: To run this script, you need to install `allennlp==0.9.0` because
+that is the last version that supports the `allennlp elmo` command.
+We do not include this in `requirements.txt` because it shares too many
+dependencies with the rest of this project, among other issues (e.g., it
+cannot be run on Python >= 3.9). I suggest creating a separate venv for this.
+'''
+
+ELMO_URL = 'https://s3-us-west-2.amazonaws.com/allennlp/models/elmo'
+ELMO_MODEL = '2x4096_512_2048cnn_2xhighway_5.5B'
+WEIGHTS_FILE = 'elmo_2x4096_512_2048cnn_2xhighway_5.5B_weights.hdf5'
+WEIGHTS_URL = f'{ELMO_URL}/{ELMO_MODEL}/{WEIGHTS_FILE}'
+OPTIONS_FILE = 'elmo_2x4096_512_2048cnn_2xhighway_5.5B_args.json'
+OPTIONS_URL = f'{ELMO_URL}/{ELMO_MODEL}/{OPTIONS_FILE}'
+
+parser = argparse.ArgumentParser(description='pre-embed sentences with elmo',
+                                 epilog=EPILOG)
+parser.add_argument('data_dir',
+                    type=pathlib.Path,
+                    help='dir containing ptb .conll(x) files')
+parser.add_argument('--out-dir',
+                    type=pathlib.Path,
+                    help='output dir (default: data_dir)')
+parser.add_argument('--word-column',
+                    type=int,
+                    default=1,
+                    help='index of .conll(x) column containing words')
+parser.add_argument('--device', help='use this device (default: guessed)')
+args = parser.parse_args()
+
+data_dir = args.data_dir
+if not data_dir.exists():
+    raise FileNotFoundError(f'data dir {data_dir} not found')
+out_dir = args.out_dir or data_dir
+
+for split in ('train', 'dev', 'test'):
+    data_file = data_dir / f'ptb3-wsj-{split}.conllx'
+    out_file = out_dir / f'elmo-{split}.h5'
+
+    # Parse the conll data.
+    # TODO(evandez): Just use the parse lib?
+    with args.data_file.open() as data:
+        lines = data.readlines()
+
+    buffer: List[str] = []
+    sentences: List[str] = []
+    for line in lines:
+        line = line.strip()
+        if not line and buffer:
+            sentences.append(' '.join(buffer))
+            buffer = []
+            continue
+        elif not line or line.startswith('#'):
+            continue
+        buffer.append(line.split()[args.word_column])
+
+    if buffer:
+        sentences.append(' '.join(buffer))
+
+    # Call the (very old) allennlp elmo command.
+    with tempfile.TemporaryDirectory() as tempdir:
+        raw = pathlib.Path(tempdir) / f'{args.data_file.name}.raw'
+        with raw.open('w') as handle:
+            handle.write('\n'.join(sentences))
+        command = ['allennlp', 'elmo', str(raw), str(args.out_file)]
+        command += ['--weight-file', WEIGHTS_URL]
+        command += ['--options-file', OPTIONS_URL]
+        command += ['--all']
+
+        device = args.device
+        if device:
+            if 'cuda' in args.device:
+                device = device.replace('cuda:', '')
+            command += ['--cuda-device', device]
+        elif cuda.is_available():
+            command += ['--cuda-device', '0']
+
+        process = subprocess.run(command)
+        if process.returncode:
+            sys.exit(process.returncode)
