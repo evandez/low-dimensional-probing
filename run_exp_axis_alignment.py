65be6a8047d437af11e1dd99e1874e87c1ce2d33
Evan Hernandez <evanfredhernandez@gmail.com>
2021-10-17 15:20:48 -0700

    Use fancy new env module.

--- run_exp_axis_alignment.py
+++ run_exp_axis_alignment.py
@@ -14,21 +14,39 @@ from ldp import datasets, tasks
 from ldp.models import probes, projections
 from ldp.parse import splits
 from ldp.tasks import dlp, pos
-from ldp.utils import logging
+from ldp.utils import env, logging
 
 import torch
 import wandb
-from torch import cuda, nn
+from torch import cuda
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--representation-model',
+parser = argparse.ArgumentParser(description='run axis alignment experiments')
+parser.add_argument('task',
+                    choices=(tasks.PART_OF_SPEECH_TAGGING,
+                             tasks.DEPENDENCY_LABEL_PREDICTION),
+                    help='task on which probe was trained')
+parser.add_argument(
+    '--data-dir',
+    type=pathlib.Path,
+    help='root dir containing data (default: project data dir)')
+parser.add_argument(
+    '--results-dir',
+    help='root dir containing trained probes (default: project results dir)')
+parser.add_argument('--linear',
+                    action='store_const',
+                    help='use linear probe (default: mlp)')
+parser.add_argument('--model',
                     choices=('elmo', 'bert', 'bert-random'),
                     default='elmo',
                     help='representations to probe (default: elmo)')
-parser.add_argument('--representation-layer',
+parser.add_argument('--layer',
                     type=int,
                     default=0,
                     help='layer to probe (default: 0)')
+parser.add_argument('--rank',
+                    type=int,
+                    default=8,
+                    help='rank of probe to use (default: 8)')
 parser.add_argument('--no-batch',
                     action='store_true',
                     help='store entire dataset in RAM/GPU and do not batch it')
@@ -41,10 +59,6 @@ parser.add_argument('--wandb-group',
                     help='experiment group (default: axis-alignment)')
 parser.add_argument('--wandb-name',
                     help='experiment name (default: generated)')
-parser.add_argument('--download-probe-to',
-                    type=pathlib.Path,
-                    help='if set, treat probe argument as wandb run path '
-                    'and restore probe from that run to this path')
 parser.add_argument(
     '--representations-key',
     default=datasets.DEFAULT_H5_REPRESENTATIONS_KEY,
@@ -52,24 +66,21 @@ parser.add_argument(
 parser.add_argument('--features-key',
                     default=datasets.DEFAULT_H5_FEATURES_KEY,
                     help='key for features dataset in h5 file (default: tags)')
-parser.add_argument('task',
-                    choices=(
-                        tasks.PART_OF_SPEECH_TAGGING,
-                        tasks.DEPENDENCY_LABEL_PREDICTION,
-                    ),
-                    help='task on which probe was trained')
-parser.add_argument('data_dir', type=pathlib.Path, help='task data dir')
-parser.add_argument('probe_file',
-                    help='path to probe weights, or wandb run path if '
-                    '--download-probe-to is set')
 args = parser.parse_args()
+
+task = args.task
+model = args.model
+layer = args.layer
+rank = args.rank
+
+# Set up environment. Do not set config yet, as we have to read the probe.
+wandb.init(project='ldp',
+           name=args.wandb_name or f'{task}-{model}-l{layer}-r{rank}',
+           group=args.wandb_group)
 
 if args.task not in (tasks.PART_OF_SPEECH_TAGGING,
                      tasks.DEPENDENCY_LABEL_PREDICTION):
     raise ValueError(f'unsupported task: {args.task}')
-
-# Set up environment. Do not set config yet, as we have to read the probe.
-wandb.init(project='lodimp', name=args.wandb_name, group=args.wandb_group)
 
 logging.configure()
 log = logging.getLogger(__name__)
@@ -77,34 +88,25 @@ log = logging.getLogger(__name__)
 device = args.device or 'cuda' if cuda.is_available() else 'cpu'
 log.info('using %s', device)
 
-probe: nn.Module
-probe_file: pathlib.Path
-if args.download_probe_to:
-    probe_file = args.download_probe_to
-    log.info('downloading probe from %s to %s', args.probe, probe_file)
-    wandb.restore(str(probe_file), run_path=str(args.probe_file))
-    assert probe_file.exists()
-else:
-    probe_file = args.probe_file
+results_root = args.results_dir or env.results_dir()
+results_dir = results_root / 'train-probe'
+results_dir /= 'linear' if args.linear else 'mlp'
+results_dir /= f'{task}/{model}/l{layer}'
+results_dir /= f'r{rank}' if rank is not None else 'full'
 
+probe_file = results_dir / 'probe.pth'
 log.info('reading probe from %s', probe_file)
 probe = torch.load(probe_file, map_location=device)
 
-if not (isinstance(probe, probes.Linear) or isinstance(probe, probes.MLP)):
-    raise ValueError(f'bad probe type: {probe.__class__}')
-projection = probe.project
-assert projection is not None, 'no projection?'
-assert isinstance(projection, projections.Projection)
-
 # Now we can set the full run config...
 wandb.config.update({
-    'task': args.task,
+    'task': task,
     'representations': {
-        'model': args.representation_model,
-        'layer': args.representation_layer,
+        'model': args.model,
+        'layer': args.layer,
     },
     'projection': {
-        'dimension': projection.out_features,
+        'rank': rank,
     },
     'probe': {
         'model': {
@@ -114,14 +116,13 @@ wandb.config.update({
     },
 })
 
-representation_model = args.representation_model
-representation_layer = args.representation_layer
-data_root = args.data_dir / representation_model / str(representation_layer)
+data_root = args.data_dir or env.data_dir()
+data_dir = data_root / 'ptb3/collated' / task / model / str(layer)
 cache = device if args.cache else None
 
 data: Dict[str, datasets.CollatedTaskDataset] = {}
 for split in splits.STANDARD_SPLITS:
-    split_path = data_root / f'{split}.h5'
+    split_path = data_dir / f'{split}.h5'
     if args.no_batch:
         data[split] = datasets.NonBatchingCollatedTaskDataset(
             split_path,
